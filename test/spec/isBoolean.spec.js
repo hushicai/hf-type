41c27fb9430aa9908c1c5b2430701c94f1c2716e
hushicai <bluthcy@gmail.com>
2015-04-13 16:43:01 +0800

    add test

---
+++ test/spec/isBoolean.spec.js
@@ -0,0 +1,24 @@
+/**
+ * @file isBoolean.spec.js
+ * @author hushicai(bluthcy@gmail.com)
+ */
+
+define(
+    function (require) {
+        var isBoolean = require('isBoolean');
+
+        describe('isBoolean test suite', function () {
+            it('should be true', function () {
+                expect(isBoolean(true)).toBe(true);
+                expect(isBoolean(false)).toBe(true);
+                /* eslint-disable no-new-wrappers */
+                expect(isBoolean(new Boolean(true))).toBe(true);
+                expect(isBoolean(new Boolean(false))).toBe(true);
+                /* eslint-enable no-new-wrappers */
+            });
+            it('should be false', function () {
+                expect(isBoolean(1)).toBe(false);
+            });
+        });
+    }
+);
