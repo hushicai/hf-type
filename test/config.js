3168db9f6d0bb043574e9b5fb6fb7d710f01d9d0
hushicai <bluthcy@gmail.com>
2015-04-13 19:36:34 +0800

    update

--- test/config.js
+++ test/config.js
@@ -1,69 +1,78 @@
-module.exports = function(config) {
-  config.set({
+/**
+ * @file karma config
+ * @author hushicai(bluthcy@gmail.com)
+ */
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../',
+module.exports = function (config) {
+    config.set({
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-        'test/main.js',
-        {pattern: 'src/**/*.js', included: false},
-        {pattern: 'test/spec/**/*.spec.js', included: false}
-    ],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '../',
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine', 'requirejs'],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-        "src/**/*.js": ["coverage"]
-    },
+        // list of files / patterns to load in the browser
+        files: [
+            'test/main.js',
+            {pattern: 'src/**/*.js', included: false},
+            {pattern: 'test/spec/**/*.spec.js', included: false}
+        ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-    coverageReporter: {
-        type: 'html',
-        dir: 'coverage/'
-    },
+        // list of files to exclude
+        exclude: [
+        ],
 
 
-    // web server port
-    port: 9876,
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'src/**/*.js': ['coverage']
+        },
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage'],
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/'
+        },
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // web server port
+        port: 9876,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+        // level of logging
+        // possible values: config.LOG_DISABLE
+        //               || config.LOG_ERROR
+        //               || config.LOG_WARN
+        //               || config.LOG_INFO
+        //               || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
+    });
 };
--- test/main.js
+++ test/main.js
@@ -1,12 +1,18 @@
+/**
+ * @file requirejs config
+ * @author hushicai(bluthcy@gmail.com)
+ */
+
 var tests = [];
 for (var file in window.__karma__.files) {
-  if (window.__karma__.files.hasOwnProperty(file)) {
-    if (/spec\.js$/.test(file)) {
-      tests.push(file);
+    if (window.__karma__.files.hasOwnProperty(file)) {
+        if (/spec\.js$/.test(file)) {
+            tests.push(file);
+        }
     }
-  }
 }
 
+/* global requirejs */
 requirejs.config({
     // Karma serves files from '/base'
     baseUrl: '/base/src',
